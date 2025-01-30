fb05e3b97b8416108421e7d392cb533d44e992bf
Sarina Naeimi <snnaeimi@gmail.com>
2025-01-29 22:37:09 -0800

    Header created

---
+++ chefApp/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header(){
+    return(
+        <div className="headerBar">
+        <img className="headerImg" src="../public/images/chef-claude-icon.png"/>
+        <p className="headeTitle">Chef Claude</p>
+        </div>
+    )
+}
