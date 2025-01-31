7554c165e3b8dfb79f2e298c627828d319c7d20b
Sarina Naeimi <snnaeimi@gmail.com>
2025-01-31 01:00:45 -0800

    add useState for form element

--- chefApp/components/Main.jsx
+++ chefApp/components/Main.jsx
@@ -1,15 +1,36 @@
+import React from "react"
+
 export default function Main(){
+    const [ingredients, setIngredients] = React.useState([])
+
+    const ingredientsListItems = ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+
+    function handleClick(event){
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+
+        setIngredients(prevIng => [...prevIng, newIngredient])
+    }
+
+
     return(
         <main>
-            <form className="formClass">
+            <form onSubmit={handleClick} className="formClass">
                 <input 
                     type="text"
                     placeholder="e.g. oregano"
                     aria-label="Add Ingrediants"
                     className="inputFeild"
+                    name="ingredient"
                 />
                 <button className="addIngredientBtn">+ Add ingredient</button>
             </form>
+            <ul>
+                {ingredientsListItems}
+            </ul>
         </main>
     )
 }
