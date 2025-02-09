8280f62d1110159be752724a73b912e87172bf11
Sarina Naeimi <snnaeimi@gmail.com>
2025-02-08 23:29:49 -0800

    added new component for ingredients data

--- chefApp/Main.jsx
+++ chefApp/Main.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import ResultRecipe from "./components/ResultRecipe";
+import IngredientsData from "./components/IngredientsData";
 
 export default function Main() {
   const [ingredients, setIngredients] = React.useState([]);
 
   const [recipeShown, setRecipeShown] = React.useState(false);
-
-  const ingredientsListItems = ingredients.map((ingredient) => (
-    <li key={ingredient}>{ingredient}</li>
-  ));
 
   function toggleRecipeShown() {
     setRecipeShown((prevRecipeShown) => !prevRecipeShown);
@@ -18,8 +15,11 @@ export default function Main() {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
     const newIngredient = formData.get("ingredient");
+    if (newIngredient !== "") {
+      setIngredients((prevIng) => [...prevIng, newIngredient]);
+    }
+
     event.target.reset();
-    setIngredients((prevIng) => [...prevIng, newIngredient]);
   }
 
   return (
@@ -34,22 +34,11 @@ export default function Main() {
         />
         <button className="addIngredientBtn">+ Add ingredient</button>
       </form>
-      {ingredients.length > 3 && (
-        <section className="ingredientsSection">
-          <h2>Ingredients on hand:</h2>
-          <ul className="ingredients-list" aria-live="polite">
-            {ingredientsListItems}
-          </ul>
-          {ingredientsListItems.length > 3 && (
-            <div className="get-recipe-container">
-              <div>
-                <h3>Ready for a recipe?</h3>
-                <p>Generate a recipe from your list of ingredients.</p>
-              </div>
-              <button onClick={toggleRecipeShown}>Get a recipe</button>
-            </div>
-          )}
-        </section>
+      {ingredients.length > 0 && (
+        <IngredientsData
+          ingredients={ingredients}
+          toggleRecipeShown={toggleRecipeShown}
+        />
       )}
       {recipeShown && <ResultRecipe />}
     </main>
